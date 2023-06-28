fe3a21133ddb9f127d5b09fcde71a321466dc845
Alisher <alishcashq@gmail.com>
2023-06-29 05:32:50 +0600

    update

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const _axios = axios.create({
-  baseURL: 'http://localhost:3000',
-
+  baseURL: `${process.env.baseUrl}`,
   headers: {
     "Content-Type": "application/json",
   },
@@ -11,7 +10,7 @@ const _axios = axios.create({
 
 _axios.interceptors.request.use((config: any) => {
   const newConfig = { ...config };
-  const userData = Cookies.get('userData');
+  const userData = Cookies.get("userData");
   const authData = userData ? JSON.parse(userData) : null;
 
   if (authData && newConfig.headers) {

9ec7efb9885336669441b29ba0c25ebafe3e94af
Alisher <alishcashq@gmail.com>
2023-06-29 06:30:29 +0600

    maybe

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -4,9 +4,9 @@ import Cookies from "js-cookie";
 const _axios = axios.create({
   // vercel env crash
   baseURL: `https://so-bes-senior-task.vercel.app/`,
-  headers: {
-    "Content-Type": "application/json",
-  },
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
 });
 
 _axios.interceptors.request.use((config: any) => {

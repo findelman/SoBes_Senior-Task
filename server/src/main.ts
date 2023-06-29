eb117713c8684a23b05e58156b9f538e1a56bf79
Alisher <alishcashq@gmail.com>
2023-06-29 07:02:41 +0600

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,13 +6,13 @@ const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const corsOptions: CorsOptions = {
-  //   origin: '*, https://sobes-task.vercel.app',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   allowedHeaders: 'Content-Type, Accept, Authorization',
-  // };
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
 
-  // app.enableCors(corsOptions);
+  app.enableCors(corsOptions);
   await app.listen(port);
 }
 bootstrap();
